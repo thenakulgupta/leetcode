7312e88d702761c068a0a51d9f92fbb5230188ca
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-03 17:14:06 +0530

    Time: 29 ms (72.73%) | Memory: 20.1 MB (91.22%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,12 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        leftMultiple = nums[0]
-        output = [a for a in nums]
-        for i in range(len(output)-2, 0, -1):
-            output[i] = output[i] * output[i+1]
-        output[0] = output[1]
-        for i in range(1, len(nums)):
-            output[i] = (1 if i == len(output)-1 else output[i+1]) * leftMultiple
-            leftMultiple *= nums[i]
+        output = [1] * len(nums)
+
+        # prefix
+        prefix = 1
+        for i in range(len(nums)):
+            output[i] = prefix
+            prefix *= nums[i]
+
+        # suffix
+        suffix = 1
+        for i in range(len(nums)-1, -1, -1):
+            output[i] *= suffix
+            suffix *= nums[i]
+
         return output
