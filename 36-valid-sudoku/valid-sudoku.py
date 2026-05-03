7aae078acbe639ff35d3d2bfbf4ed38ebe018475
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-03 17:29:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (94.93%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,30 +1,20 @@
-class Solution(object):
-    def checkIsValidNumber(self, board, i, j, number):
-        sqRowNumber = (i // 3) * 3
-        sqColNumber = (j // 3) * 3
-
-        for n in range(9):
-            if board[i][n] == number and n != j:
-                return False
-            if board[n][j] == number and n != i:
-                return False
-
-        for m in range(3):
-            for n in range(3):
-                if (sqRowNumber + m != i or sqColNumber + n != j) and board[sqRowNumber + m][sqColNumber + n] == number:
-                    return False
-        return True
-
+class Solution:
     def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
+        rows = set()
+        cols = set()
+        boxes = set()
+
         for i in range(9):
             for j in range(9):
-                k = board[i][j]
-                if k != ".":
-                    num = str(k)
-                    if not self.checkIsValidNumber(board, i, j, num):
-                        return False
+                val = board[i][j]
+                if val == ".":
+                    continue
+
+                if (i, val) in rows or (j, val) in cols or ((i//3, j//3), val) in boxes:
+                    return False
+
+                rows.add((i, val))
+                cols.add((j, val))
+                boxes.add(((i//3, j//3), val))
+
         return True
