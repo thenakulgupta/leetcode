250aac5782b9558dcada7100ea3fbcb8b140ba5b
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-03 18:10:55 +0530

    Time: 83 ms (4.13%) | Memory: 19.5 MB (6.01%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isClosable(self, v, s):
+        if v == "(" and s == ")":
+            return True
+        elif v == "[" and s == "]":
+            return True
+        elif v == "{" and s == "}":
+            return True
+        return False
+
+    def isValid(self, s: str) -> bool:
+        if len(s) == 1 or len(s) % 2 != 0:
+            return False
+        arr = [_ for _ in s]
+        openParentheses = ["(", "[", "{"]
+        i = 0
+        while True:
+            if i >= len(arr):
+                break
+            if arr[i] not in openParentheses and i != 0:
+                if not self.isClosable(arr[i-1], arr[i]):
+                    return False
+                else:
+                    arr = arr[:i-1] + arr[i+1:]
+                    i -= 2
+            i += 1
+        print(arr)
+        return len(arr) == 0
