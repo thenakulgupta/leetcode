5fb645baccb5edf231ea7cc5eb2c32d7248afc1c
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-08 14:15:23 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (53.29%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,53 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    def build(self, preorder, preL, preR, inorder, inL, inR, hm):
+    def build(self, preL, preR, inL, inR, preorder, inorder, hm):
         if preL > preR or inL > inR:
             return None
 
         rootVal = preorder[preL]
         root = TreeNode(rootVal)
+        inOrderIndex = hm[rootVal]
+        leftSize = inOrderIndex - inL
 
-        inorderIndex = hm[rootVal]
-
-        leftSize = inorderIndex - inL
-
-        root.left = self.build(
-            preorder,
-            preL + 1,
-            preL + leftSize,
-            inorder,
-            inL,
-            inorderIndex - 1,
-            hm
-        )
-
-        root.right = self.build(
-            preorder,
-            preL + leftSize + 1,
-            preR,
-            inorder,
-            inorderIndex + 1,
-            inR,
-            hm
-        )
-
+        root.left = self.build(preL + 1, preL + leftSize, inL, inOrderIndex - 1, preorder, inorder, hm)
+        root.right = self.build(preL + leftSize + 1, preR, inOrderIndex + 1, inR, preorder, inorder, hm)
         return root
-
+        
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         hm = {}
-
-        for i, val in enumerate(inorder):
-            hm[val] = i
-
-        return self.build(
-            preorder,
-            0,
-            len(preorder) - 1,
-            inorder,
-            0,
-            len(inorder) - 1,
-            hm
-        )
+        n = len(inorder)
+        for i in range(n):
+            hm[inorder[i]] = i
+        return self.build(0, n - 1, 0, n - 1, preorder, inorder, hm)
