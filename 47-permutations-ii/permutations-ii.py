8798800efd09b2b96585b5bb34eb8c2935e5d531
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-13 00:18:50 +0530

    Time: 3 ms (90.44%) | Memory: 20 MB (21.04%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,17 +1,18 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        self.retArr = []
         nums = sorted(nums)
-        retArr=[]
-        def helper(nums, arr=[]):
+
+        def permuteUniqueHelper(nums, arr=[]):
             if 0 == len(nums):
-                nonlocal retArr
-                retArr.append(arr[:])
+                self.retArr.append(arr[:])
+                return
 
             for i in range(len(nums)):
-                if nums[i] != (nums[i + 1] if i + 1 < len(nums) else nums[i] - 1):
+                if nums[i] != (nums[i-1] if i > 0 else nums[i] - 1):
                     arr.append(nums[i])
-                    helper(nums[:i] + nums[i+1:], arr)
+                    permuteUniqueHelper(nums[:i] + nums[i+1:], arr)
                     arr.pop()
 
-        helper(nums)
-        return retArr
+        permuteUniqueHelper(nums)
+        return self.retArr
