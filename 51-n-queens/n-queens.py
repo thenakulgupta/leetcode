7dc85d13ac4248005f8a74ba6811a50ad59dfb5e
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-12 20:45:37 +0530

    Time: 45 ms (8.67%) | Memory: 19.7 MB (68.46%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,56 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["." for _ in range(n)] for _ in range(n)]
+        self.finalBoard = []
+
+        def isValidPosition(board, i, j):
+            n = len(board)
+            for row in range(n):
+                if i != row and board[row][j] == 'Q':
+                    return False
+            for col in range(n):
+                if j != col and board[i][col] == 'Q':
+                    return False
+            row, col = i - 1, j - 1
+            while row >= 0 and col >= 0:
+                if board[row][col] == 'Q':
+                    return False
+                row -= 1
+                col -= 1
+            row, col = i - 1, j + 1
+            while row >= 0 and col < n:
+                if board[row][col] == 'Q':
+                    return False
+                row -= 1
+                col += 1
+            row, col = i + 1, j - 1
+            while row < n and col >= 0:
+                if board[row][col] == 'Q':
+                    return False
+                row += 1
+                col -= 1
+            row, col = i + 1, j + 1
+            while row < n and col < n:
+                if board[row][col] == 'Q':
+                    return False
+                row += 1
+                col += 1
+            return True
+
+
+        def nQueens(board, i = 0):
+            if i == n:
+                self.finalBoard.append(["".join(_) for _ in board])
+                return True
+            
+            for j in range(n):
+                if isValidPosition(board, i, j):
+                    board[i][j] = 'Q'
+                    nQueens(board, i + 1)
+                    board[i][j] = '.'
+
+            return False
+
+
+        nQueens(board)
+        return self.finalBoard
