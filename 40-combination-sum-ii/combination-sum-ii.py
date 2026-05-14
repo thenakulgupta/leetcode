4422378edd29fe8374e4d56a0f8ac28aece423be
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-14 16:00:58 +0530

    Time: 28 ms (23.86%) | Memory: 19.5 MB (42.22%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,10 +1,11 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates = sorted(candidates)
-        self.retAns = []
-        def helper(candidates, target, currentSum = 0, arr = [], i = 0):
-            if currentSum == target:
-                self.retAns.append(arr[:])
+        self.retArr = []
+
+        def combinationSum2Helper(candidates, target, currentSum, i, arr):
+            if target == currentSum:
+                self.retArr.append(arr[:])
                 return
 
             if i == len(candidates):
@@ -13,16 +14,15 @@ class Solution:
             if currentSum > target:
                 return
 
-            arr.append(candidates[i])
             currentSum += candidates[i]
-            helper(candidates, target, currentSum, arr, i + 1)
+            arr.append(candidates[i])
+            combinationSum2Helper(candidates, target, currentSum, i + 1, arr)
 
-            arr.pop()
             currentSum -= candidates[i]
+            arr.pop()
             while i < len(candidates) - 1 and candidates[i] == candidates[i + 1]:
                 i += 1
-            helper(candidates, target, currentSum, arr, i + 1)
+            combinationSum2Helper(candidates, target, currentSum, i + 1, arr)
 
-
-        helper(candidates, target)
-        return self.retAns
+        combinationSum2Helper(candidates, target, 0, 0, [])
+        return self.retArr
