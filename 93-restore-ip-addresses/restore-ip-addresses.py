f8e9bec1fda8e9bc8da8551dc0aec0a5d44b8dbb
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-14 11:58:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (68.35%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -0,0 +1,44 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        if len(s) < 4 or len(s) > 12:
+            return []
+        self.retAns = []
+        def checkIP(arr):
+            maxToBeAdded = 4 * 3 - len(arr) * 3
+            leftNow = (4 - len(arr)) * 3
+
+            if leftNow > maxToBeAdded:
+                return False
+
+            if len(arr) > 4:
+                return False
+
+            if len(arr) > 0:
+                lastEle = arr[-1]
+                if len(lastEle) != len(str(int(lastEle))):
+                    return False
+                if int(lastEle) < 0 or int(lastEle) > 255:
+                    return False
+            return True
+        def restoreIpAddressesHelper(s, i=0, arr=[]):
+            if i == len(s):
+                if len(arr) == 4 and sum([len(_) for _ in arr]) == len(s) and checkIP(arr):
+                    self.retAns.append(".".join(arr))
+                return True
+
+            if not checkIP(arr):
+                return False
+
+            arr.append("")
+            for j in range(i, min(i + 3, len(s))):
+                lastEle = arr[-1]
+                lastEle += s[j]
+                arr[-1] = lastEle
+                if not restoreIpAddressesHelper(s, j + 1, arr):
+                    lastEle = lastEle[:-1]
+            arr.pop()
+            return True
+            
+
+        restoreIpAddressesHelper(s)
+        return self.retAns
