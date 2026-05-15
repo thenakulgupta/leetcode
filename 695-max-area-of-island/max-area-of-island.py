b5a82ccff115d19838490c7a2e9ad44045782d6a
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-15 23:20:29 +0530

    Time: 36 ms (7.69%) | Memory: 19.7 MB (78.39%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,46 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        self.ans = 0
+        self.visited = set()
+        self.graph = deque()
+
+        def isSafe(grid, i, j):
+            return not (i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]))
+
+        def bfs(grid, i, j):
+            if grid[i][j] != 1:
+                return
+
+            if (i, j) in self.visited:
+                return
+
+
+            self.visited.add((i, j))
+            self.graph.append((i, j))
+
+            dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+            _len = 0
+            while len(self.graph):
+                i1, j1 = self.graph.popleft()
+                if not isSafe(grid, i1, j1):
+                    continue
+
+                _len += 1
+
+                for _dir in dirs:
+                    i2, j2 = _dir
+                    newI = i1 + i2
+                    newJ = j1 + j2
+                    if isSafe(grid, newI, newJ) and grid[newI][newJ] == 1 and (newI, newJ) not in self.visited:
+                        self.visited.add((newI, newJ))
+                        self.graph.append((newI, newJ))
+
+            self.ans = max(self.ans, _len)
+
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                bfs(grid, i, j)
+
+        return self.ans
