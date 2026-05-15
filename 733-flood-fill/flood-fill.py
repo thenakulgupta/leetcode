c4a3ca816b7ac1d9d4544ccf83dd5a98cb46fc3b
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-15 21:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (84.69%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,18 +1,23 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        stColor = image[sr][sc]
-        def floodFillHelper(image, sr, sc, color, stColor):
-            if sr < 0 or sc < 0 or sr >= len(image) or sc >= len(image[0]):
-                return
-            if image[sr][sc] != stColor or image[sr][sc] == color:
-                return
-
-            directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-            image[sr][sc] = color
-            for direction in directions:
-                row, col = direction
-                floodFillHelper(image, sr + row, sc + col, color, stColor)
-
-
-        floodFillHelper(image, sr, sc, color, stColor)
+        graph = deque([(sr, sc)])
+        visited = set()
+        visited.add((sr, sc))
+        startingPixel = image[sr][sc]
+        dirs = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        def isSafe(image, i, j):
+            return not (i < 0 or j < 0 or i >= len(image) or j >= len(image[i]))
+        
+        while len(graph):
+            i, j = graph.popleft()
+            if image[i][j] != startingPixel:
+                continue
+            image[i][j] = color
+            for _dir in dirs:
+                i1, j1 = _dir
+                newI = i + i1
+                newJ = j + j1
+                if isSafe(image, newI, newJ) and (newI, newJ) not in visited:
+                    graph.append((newI, newJ))
+                    visited.add((newI, newJ))
         return image
