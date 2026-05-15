7295bc90018d646c0ebb03fa7f974a90ab5fd697
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-15 23:34:49 +0530

    Time: 301 ms (5.78%) | Memory: 30.8 MB (12.39%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,13 +1,15 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         self.visited = set()
-        self.graph = deque()
         self.ans = 0
 
         def isSafe(grid, i, j):
             return not (i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]))
 
-        def bfs(grid, i, j):
+        def dfs(grid, i, j, isFirst = True):
+            if not isSafe(grid, i, j):
+                return
+
             if grid[i][j] != '1':
                 return
 
@@ -15,23 +17,19 @@ class Solution:
                 return
 
             self.visited.add((i, j))
-            self.graph.append((i, j))
             dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            while len(self.graph):
-                i1, j1 = self.graph.popleft()
-                for _dir in dirs:
-                    i2, j2 = _dir
-                    newI = i1 + i2
-                    newJ = j1 + j2
-                    if isSafe(grid, newI, newJ) and (newI, newJ) not in self.visited and grid[newI][newJ] == '1':
-                        self.graph.append((newI, newJ))
-                        self.visited.add((newI, newJ))
-
-            self.ans += 1
+            if isFirst:
+                self.ans += 1
+                isFirst = False
+            for _dir in dirs:
+                i2, j2 = _dir
+                newI = i + i2
+                newJ = j + j2
+                dfs(grid, newI, newJ, isFirst)
 
 
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                bfs(grid, i, j)
+                dfs(grid, i, j)
         
         return self.ans
