be2dbc99d9dff278a6f1839ab21b1a9dd1085ccb
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-16 23:22:46 +0530

    Time: 278 ms (11.11%) | Memory: 33.4 MB (81.30%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,13 +1,20 @@
 class NumMatrix:
 
+    def getSafeValue(self, row, col):
+        matrix = self.matrix
+        return 0 if row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0]) else matrix[row][col]
+
     def __init__(self, matrix: List[List[int]]):
-        self.matrixSum = [[0] * (len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
-        for i in range(1, len(matrix) + 1):
-            for j in range(1, len(matrix[0]) + 1):
-                self.matrixSum[i][j] = matrix[i - 1][j - 1] + self.matrixSum[i-1][j] + self.matrixSum[i][j-1] - self.matrixSum[i - 1][j - 1]
+        self.matrix = [[0] * (len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
+
+        for row in range(1, len(self.matrix)):
+            for col in range(1, len(self.matrix[0])):
+                self.matrix[row][col] = matrix[row - 1][col - 1] + self.getSafeValue(row - 1, col) + self.getSafeValue(row, col - 1) - self.getSafeValue(row - 1, col - 1)
+
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.matrixSum[row2 + 1][col2 + 1] - self.matrixSum[row2 + 1][col1] - self.matrixSum[row1][col2 + 1] + self.matrixSum[row1][col1]
+        return self.getSafeValue(row2 + 1, col2 + 1) - self.getSafeValue(row1, col2 + 1) - self.getSafeValue(row2 + 1, col1) + self.getSafeValue(row1, col1)
+
 
 
 # Your NumMatrix object will be instantiated and called as such:
