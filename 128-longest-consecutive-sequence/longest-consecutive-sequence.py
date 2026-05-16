07ab40ff068f1c195e1d26828b7813dc8bb1eaa1
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-17 00:23:08 +0530

    Time: 84 ms (11.36%) | Memory: 42.5 MB (6.38%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,20 +1,18 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        hashmaps = {}
-        results = 0
+        hm = {}
+        visited = set()
         for num in nums:
-            hashmaps[num] = num
-        for num in hashmaps.keys():
-            if num-1 not in hashmaps:
-                i = 0
-                localResult = 0
-                while True:
-                    a = num + i
-                    if a in hashmaps:
-                        localResult += 1
-                    else:
-                        if results < localResult:
-                            results = localResult
-                        break
-                    i+=1
-        return results
+            hm[num] = 1
+
+        ans = 0
+        for num in nums:
+            if num - 1 not in hm and num not in visited:
+                curr = num
+                curr_len = 0
+                while curr in hm:
+                    curr_len += 1
+                    visited.add(curr)
+                    curr += 1
+                ans = max(ans, curr_len)
+        return ans
