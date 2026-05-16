a09147469ed5d31c98cfb94a655dd82a955c9b84
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-16 21:31:19 +0530

    Time: 165 ms (12.96%) | Memory: 77.1 MB (46.97%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,78 +1,103 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
         self.next = None
         self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.head = None
-        self.last = None
-        self.ll_by_key = {}
         self.capacity = capacity
+        self.cache_count = 0
+        self.cache_head = None
+        self.cache_tail = None
+        self.cache_hm = {}
 
-
-    def prependToLL(self, data):
-        new_node = Node(data)
-        self.removeFromLL(data[0])
-        if self.head is None:
-            self.ll_by_key[data[0]] = new_node
-            self.head = new_node
-            self.last = new_node
-            return
-        if len(self.ll_by_key.keys()) >= self.capacity:
-            last = self.last
-            last_second = last.prev if last and last.prev else None
-            if(last_second):
-                if last.data[0] in self.ll_by_key:
-                    del self.ll_by_key[last.data[0]]
-                last_second.next = None
-                self.last = last_second
-            else:
-                self.head = None
-                self.last = None
-                self.ll_by_key = {}
-        self.ll_by_key[data[0]] = new_node
-        first = self.head
-        self.head = new_node
-        self.head.next = first
-        if first:
-            first.prev = new_node
-            if len(self.ll_by_key.keys()) == 2:
-                self.last = first
-
-
-    def removeFromLL(self, key):
-        node = self.ll_by_key[key] if key in self.ll_by_key else None
-        if node:
-            prev = node.prev
-            if prev:
-                prev.next = node.next
-                if node.next:
-                    node.next.prev = prev
-                else:
-                    self.last = prev
-            else:
-                _next = self.head.next if self.head else None
-                if _next:
-                    _next.prev = None
-                self.head = _next
-            del self.ll_by_key[key]
-            if len(self.ll_by_key.keys()) <= 1:
-                self.last = self.head
-
+    def printCache(self, val):
+        # node = self.cache_head
+        # print(val, " ", end="")
+        # while node:
+        #     print(node.val, "-> ", end="")
+        #     node = node.next
+        # print()
+        pass
+        
 
     def get(self, key: int) -> int:
-        value = self.ll_by_key[key] if key in self.ll_by_key else None
-        if not value:
-            return -1
-        self.prependToLL([key, value.data[1]])
-        return value.data[1]
+        self.printCache("get")
+        node = self.cache_hm[key] if key in self.cache_hm else None
+        if node:
+            self.remove(key)
+            self.insert(key, node.val)
+        self.printCache("get1")
+        return node.val if node else -1
+
 
     def put(self, key: int, value: int) -> None:
-        _value = self.ll_by_key[key] if key in self.ll_by_key else None
-        self.prependToLL([key, value])
+        self.printCache("put")
+        if self.cache_count >= self.capacity and key not in self.cache_hm:
+            self.remove_last()
+        else:
+            self.remove(key)
+        self.insert(key, value)
+        self.printCache("put1")
+
+
+    def insert(self, key, val):
+        self.printCache("insert")
+        new_cache = Node(key, val)
+        node = self.cache_head
+        self.cache_head = new_cache
+        new_cache.next = node
+        if new_cache.next:
+            new_cache.next.prev = new_cache
+        if not node:
+            self.cache_tail = new_cache
+        self.cache_hm[key] = new_cache
+        self.cache_count += 1
+
+
+    def remove(self, key):
+        self.printCache("remove")
+        if key not in self.cache_hm:
+            return
+
+        node = self.cache_hm[key]
+        prev = node.prev
+        _next = node.next
+        if prev:
+            prev.next = _next
+        else:
+            self.cache_head = _next
+        if _next:
+            _next.prev = prev
+        else:
+            self.cache_tail = prev
+        if not prev and not _next:
+            self.cache_head = None
+            self.cache_tail = None
+        del node
+        del self.cache_hm[key]
+        self.cache_count -= 1
+
+
+    def remove_last(self):
+        self.printCache("remove_last")
+        node = self.cache_tail
+        if not node:
+            return
+        prev = node.prev
+        if prev:
+            self.cache_tail = prev
+            prev.next = None
+        else:
+            self.cache_head = None
+            self.cache_tail = None
+
+        self.cache_count -= 1
+        key = node.key
+        del self.cache_hm[key]
 
 
 # Your LRUCache object will be instantiated and called as such:
