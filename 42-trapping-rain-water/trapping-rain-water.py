3a20a4d4d009d1145227923d13466b64bed0db07
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-17 20:57:25 +0530

    Time: 19 ms (17.29%) | Memory: 21 MB (49.91%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,26 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        i, j = 0, 1
-        totalWater = 0
-        highFromLeft = [0] * len(height)
-        highFromRight = [0] * len(height)
-        highFromLeft[0] = height[0]
-        highFromRight[len(height)-1] = height[len(height)-1]
+        leftArr, rightArr = [0] * len(height), [0] * len(height)
+        leftArr[0] = height[0]
+        rightArr[-1] = height[-1]
+        ans = 0
 
-        for m in range(1, len(height)):
-            if height[m] > highFromLeft[m - 1]:
-                highFromLeft[m] = height[m]
-            else:
-                highFromLeft[m] = highFromLeft[m - 1]
+        for i in range(1, len(height)):
+            leftArr[i] = max(leftArr[i - 1], height[i])
+        for i in range(len(height) - 2, -1, -1):
+            rightArr[i] = max(height[i], rightArr[i + 1])
+        for i in range(1, len(height) - 1):
+            ans += min(leftArr[i], rightArr[i]) - height[i]
 
-        for m in range(len(height) - 2, -1, -1):
-            if height[m] > highFromRight[m + 1]:
-                highFromRight[m] = height[m]
-            else:
-                highFromRight[m] = highFromRight[m + 1]
-
-        for m in range(1, len(height) - 1):
-            _min = min(highFromLeft[m], highFromRight[m])
-            if _min > height[m]:
-                totalWater += _min - height[m]
-        return totalWater
+        return ans
