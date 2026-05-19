505557d01b49dc671600b4b49e12d96fdfae999b
Nakul Gupta <nakulgupta1042@gmail.com>
2026-05-19 17:29:38 +0530

    Time: 45 ms (19.05%) | Memory: 21.2 MB (58.87%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,33 +1,32 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        m, n = len(heights), len(heights[0])
         ans = []
+        pacific = set()
+        atlantic = set()
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         def isSafe(heights, i, j):
             return not (i < 0 or j < 0 or i >= len(heights) or j >= len(heights[i]))
 
-        def isOceanSide(heights, i, j, _type):
-            if _type == "pacific" and (j == 0 or i == 0):
-                return True
-            if _type == "atlantic" and (j == len(heights[i]) - 1 or i == len(heights) - 1):
-                return True
-            return False
-
-        def dfs(heights, i, j, visited, _type):
+        def dfs(heights, i, j, visited):
             visited.add((i, j))
-            dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-            if isOceanSide(heights, i, j, _type):
-                return True
 
             for r, c in dirs:
                 newR = i + r
                 newC = j + c
-                if isSafe(heights, newR, newC) and (newR, newC) not in visited and heights[newR][newC] <= heights[i][j]:
-                    if dfs(heights, newR, newC, visited, _type):
-                        return True
-            return False
+                if isSafe(heights, newR, newC) and (newR, newC) not in visited and heights[newR][newC] >= heights[i][j]:
+                    dfs(heights, newR, newC, visited)
 
-        for i in range(len(heights)):
-            for j in range(len(heights[i])):
-                if dfs(heights, i, j, set(), "pacific") and dfs(heights, i, j, set(), "atlantic"):
+        for i in range(m):
+            dfs(heights, i, 0, pacific)
+            dfs(heights, i, n - 1, atlantic)
+
+        for i in range(n):
+            dfs(heights, 0, i, pacific)
+            dfs(heights, m - 1, i, atlantic)
+
+        for i in range(m):
+            for j in range(n):
+                if (i, j) in pacific and (i, j) in atlantic:
                     ans.append([i, j])
         return ans
